2e763f76a0600d9cf7a43b859559952bd27e51c0
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-15 19:06:13 +0100

    Add dav module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config.json');
 const WebUntis = require('webuntis');
-const caldav = require("node-caldav-mod");
+const dav = require('dav');
 const fs = require('fs');
 
 // Add webuntis server url if needed
@@ -8,55 +8,18 @@ if (!config.webuntis.server.includes('.')) config.webuntis.server = config.webun
 
 const untis = new WebUntis(config.webuntis.school, config.webuntis.username, config.webuntis.password, config.webuntis.server);
 
+async function sendToCal() {
+    var  xhr = new  dav.transport.Basic(new dav.Credentials({ username: config.caldav.username, password: config.caldav.password })); 
+    dav.createAccount({ server: config.caldav.url, xhr: xhr, }).then(function(account)  {
+        account.calendars.forEach(function(calendar) {
+            if (calendar.displayName == config.caldav.calendar) {
+                dav.createCalendarObject(calendar, {
 
-untis.login().then(() => {
-    untis.getOwnTimetableForToday().then((timetable) => {
-        let class_list = timetable;
-        let class_list_edit = [];
-        class_list.forEach((lesson) => {
-            lesson.date = lesson.date.toString().slice(0, 4) + "-" + lesson.date.toString().slice(4, 6) + "-" + lesson.date.toString().slice(6, 8);
-            if (lesson.startTime.toString().length == 3) lesson.startTime = "0" + lesson.startTime;
-            if (lesson.endTime.toString().length == 3) lesson.endTime = "0" + lesson.endTime;
-            lesson.startTime = lesson.startTime.toString().slice(0, 2) + ":" + lesson.startTime.toString().slice(2, 4);
-            lesson.endTime = lesson.endTime.toString().slice(0, 2) + ":" + lesson.endTime.toString().slice(2, 4);
-            class_list_edit.push({
-                "id": lesson.id,
-                "date": lesson.date,
-                "startTime": lesson.startTime,
-                "endTime": lesson.endTime,
-                "name": lesson.su[0].longname,
-                "teacher": lesson.te[0].longname,
-                "room": lesson.ro[0].name
-            });
-        });
-        class_list = class_list_edit;
-        class_list_edit = [];
-        class_list.sort((a, b) => {
-            if (a.startTime < b.startTime) return -1;
-            if (a.startTime > b.startTime) return 1;
-            return 0;
-        });
-        for (let i = 0; i < class_list.length; i++) {
-            if (class_list[i + 1] != undefined) {
-                if (class_list[i].name == class_list[i + 1].name && class_list[i].teacher == class_list[i + 1].teacher && class_list[i].room == class_list[i + 1].room) {
-                    class_list[i].endTime = class_list[i + 1].endTime;
-                    class_list.splice(i + 1, 1);
-                    i--;
-                }
+                })
             }
-        }
-        class_list.forEach((lesson) => {
-            class_list_edit.push({
-                "id": lesson.id,
-                "date": lesson.date,
-                "startTime": lesson.startTime,
-                "endTime": lesson.endTime,
-                "name": lesson.name,
-                "teacher": lesson.teacher,
-                "room": lesson.room
-            });
         });
-        class_list = class_list_edit;
-        class_list_edit = [];
     });
-});
+}
+
+
+sendToCal();
